64247f0b6cf48e61ce32a435c20f2233a0020ea7
Yoshiedev <yoshi910105@gmail.com>
2024-01-31 11:26:29 -0300

    firstcommit

---
+++ src/calculadoraNumerosEnteros/Calculadora.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadoraNumerosEnteros;
+
+/**
+ *
+ * @author YOSHIE
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
